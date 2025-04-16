3d77d0852a03997921c2fce4ceb6096dfa7bbb64
rusakov <rusakov@greensight.ru>
2025-04-16 17:19:56 +0300

    feat: errors handler

--- backend/src/services/vacations.service.ts
+++ backend/src/services/vacations.service.ts
@@ -3,13 +3,21 @@ import { PrismaClient, Vacation } from "@prisma/client";
 export default class VacationsService {
     private prisma = new PrismaClient();
 
-    async getVacations(): Promise<Vacation[]> {
-        return this.prisma.vacation.findMany();
+    async getVacations(): Promise<Vacation[] | undefined> {
+        try {
+            return await this.prisma.vacation.findMany();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    async createVacation(vacation: Vacation): Promise<Vacation> {
-        return this.prisma.vacation.create({
-            data: vacation
-        })
+    async createVacation(vacation: Vacation): Promise<Vacation | undefined> {
+        try {
+            return await this.prisma.vacation.create({
+                data: vacation
+            })
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
