97bfc93644a5f0fc41e8c13e65c99db49972ceda
rusakov <rusakov@greensight.ru>
2025-04-16 10:33:11 +0300

    feat: added envs for setting port

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { vacationRouter } from "./src/controllers/vacations.controller.js"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 
@@ -8,8 +11,14 @@ async function main() {
 
     app.use('/api', vacationRouter)
 
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+    app.use(function (req, res,) {
+        res.status(404);
+
+        res.json({ error: 'Not found' });
+    });
+
+    app.listen(process.env.NODE_PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.NODE_PORT || 3000}`);
     })
 }
 
