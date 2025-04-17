33af7c48104c3d4835f3c7de1b6de937fd7eaa66
rusakov <rusakov@greensight.ru>
2025-04-17 15:40:28 +0300

    feat[backend]: no cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import { vacancyRouter } from "./controllers/vacancy.controller";
 import { PrismaClient } from "@prisma/client"
+import cors from "cors";
 
 dotenv.config()
 const prismaClient = new PrismaClient();
@@ -9,6 +10,7 @@ const app = express();
 
 async function main() {
     app.use(express.json());
+    app.use(cors());
 
     app.use('/api', vacancyRouter)
 
