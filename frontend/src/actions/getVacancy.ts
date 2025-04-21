9b3b7c91ca9bd1dc0574a7f36f8e89b8b5d6ea32
rusakov <rusakov@greensight.ru>
2025-04-21 09:36:19 +0300

    refactor[getVacancy]: updated return value from function

--- frontend/src/actions/getVacancy.ts
+++ frontend/src/actions/getVacancy.ts
@@ -1,5 +1,5 @@
 import { IVacancy } from "@/schemas/interfaces/vacancy"
 
-export const getVacancy = async (vacancyId: string): Promise<IVacancy> => {
+export const getVacancy = async (vacancyId: string): Promise<IVacancy | { error: string }> => {
     return await fetch(`http://localhost:3001/api/vacancies/${vacancyId}`).then(res => res.json())
 }
