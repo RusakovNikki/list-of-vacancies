5611f235c5e005b2c84ffb6646b1f0311803ccc4
rusakov <rusakov@greensight.ru>
2025-04-23 12:12:57 +0300

    build[frontend]: added build analyzer

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,9 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
@@ -26,4 +32,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
