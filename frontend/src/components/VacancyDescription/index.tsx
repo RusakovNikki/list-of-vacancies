6a0e025386553acbbad8574ba1c17ec86cd18933
rusakov <rusakov@greensight.ru>
2025-04-23 15:55:35 +0300

    fix[frontend/VacancyDescription]: correct classNames

--- frontend/src/components/VacancyDescription/index.tsx
+++ frontend/src/components/VacancyDescription/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Parser from "html-react-parser";
-import { useState } from "react";
 
 interface IVacancyDescriptionProps {
   name: string;
@@ -11,25 +10,13 @@ interface IVacancyDescriptionProps {
 const VacancyDescription = (props: IVacancyDescriptionProps) => {
   const { description, name } = props;
 
-  const [showMoreDesc, setShowMoreDesc] = useState(true);
-
   return (
-    <div className="jobs-container__flex-item">
-      <div className="jobs-container__desc">
-        <div className="jobs-container__title roboto">{name}</div>
-        <div
-          className={`jobs-container__specifics ${
-            showMoreDesc ? "jobs-container__specifics--height" : ""
-          } roboto`}
-        >
+    <div className="vacancy-list__flex-item">
+      <div className="vacancy-list__desc">
+        <div className="vacancy-list__title roboto">{name}</div>
+        <div className={`vacancy-list__specifics vacancy-list__specifics--height roboto`}>
           {Parser(String(description || ""))}
         </div>
-      </div>
-      <div
-        className="jobs-container__more-btn roboto"
-        onClick={() => setShowMoreDesc((prev) => !prev)}
-      >
-        {showMoreDesc ? `more details` : "close"}
       </div>
     </div>
   );
